880f9e4c406f213d022c551e6db4e95c1b22c4a3
jerome244 <10485@holbertonstudents.com>
2025-05-05 16:01:18 +0200

    fix: pycode style checking for all

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-print("Programming is like building a multilingual puzzle")
+
+"""
+This script prints a message to the standard output.
+
+that demonstrates building a multilingual puzzle.
+"""
+print("Programming is like building a multilingual puzzle\n")
