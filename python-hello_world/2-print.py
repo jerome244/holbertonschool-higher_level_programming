489756283c9a19ced77870ebaf30ece9ca078ddf
jerome244 <10485@holbertonstudents.com>
2025-05-07 09:42:26 +0200

    fix: add newline

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -5,4 +5,4 @@ This script prints a message to the standard output.
 
 that demonstrates building a multilingual puzzle.
 """
-print("\"Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle\n")
