897d1676e4a572afcadaf882b1161447a8286c16
jerome244 <10485@holbertonstudents.com>
2025-05-07 09:57:39 +0200

    fix: rm double quotes

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 
-"""
-This script prints a message to the standard output.
-
-that demonstrates building a multilingual puzzle.
-"""
-print("\"Programming is like building a multilingual puzzle\n")
+print(\"Programming is like building a multilingual puzzle\n)
