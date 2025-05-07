b7c64389f65a4e822152a34ba8bf3aaaa4f60c1a
root <10485@holbertstudents.com>
2025-05-07 16:50:40 +0000

    Fix Check 3: normalize path to .pyc file

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-import marshal
 import sys
+import marshal
+import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.exit(1)
-    with open(sys.argv[1], 'rb') as f:
+
+    pyc_path = sys.argv[1]
+    if not os.path.isabs(pyc_path):
+        pyc_path = os.path.join(os.getcwd(), pyc_path)
+
+    with open(pyc_path, 'rb') as f:
         f.seek(16)
         code = marshal.load(f)
 
     names = [name for name in code.co_names if not name.startswith('__')]
     for name in sorted(names):
-        print(code.co_names)
+        print(name)
