e934f29533c5ad0556bc94931ce9f7523e5986d1
jerome244 <10485@holbertonstudents.com>
2025-05-07 15:23:33 +0200

    fix: rm f-string

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(10):  # First digit (0 to 9)
-    for j in range(i + 1, 10):  # Second digit (greater than the first digit)
-        if i != j:
-            print(f"{i}{j}", end=", " if i < 8 or j < 9 else "\n")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
