48b4e714db48c5989c8b094277d6aaf9812fb815
jerome244 <10485@holbertonstudents.com>
2025-05-07 15:19:43 +0200

    fix: rm f-strings

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(100):  # Loop through numbers from 0 to 99
+for i in range(100):
     if i != 99:
-        print(f"{i:02}", end=", ")  # Print with leading zero if necessary
+        print("{:02}".format(i), end=", ")
     else:
-        print(f"{i:02}")  # Print the last number without a comma
+        print("{:02}".format(i))
