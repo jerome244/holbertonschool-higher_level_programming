4145f0c29b1f283ba514c0fa201cd041abbacffe
jerome244 <10485@holbertonstudents.com>
2025-05-07 15:29:50 +0200

    fix: rewrite the code for handling no more than 2 print()

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            print(chr(ord(c) - 32), end="")
-        else:
-            print(c, end="")
-    print()
+    print("{}".format("".join(
+        chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in str)))
