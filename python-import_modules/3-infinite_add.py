b4c260aaf557b2986ff82092a85d9f2c5aa467f1
jerome244 <10485@holbertonstudents.com>
2025-05-07 16:08:04 +0200

    fix: englobe code into main if present

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-
-def main():
-    # Starting sum as 0
-    total = 0
-
-    # Loop through all arguments (starting from index 1 to skip the script name)
-    for arg in sys.argv[1:]:
-        total += int(arg)  # Convert each argument to integer and add to total
-
-    print(total)  # Print the result
-
-
 if __name__ == "__main__":
-    main()
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
