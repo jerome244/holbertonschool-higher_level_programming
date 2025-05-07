4ea4ccf57eb09667888b8396e504901436a9f96b
root <10485@holbertstudents.com>
2025-05-07 17:14:18 +0000

    fix change code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-import hidden_4
+import sys
+import marshal
 
-methods = dir(hidden_4)
-for names in methods:
-    if names[0] != '_':
-        print("{}".format(names))
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    with open(sys.argv[1], 'rb') as f:
+        f.seek(16)  # Skip header
+        code = marshal.load(f)
+
+    names = [name for name in code.co_names if not name.startswith('__')]
+    for name in sorted(names):
+        print(name)
+
