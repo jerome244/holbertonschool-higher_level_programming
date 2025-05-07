d30ee36460707fae949fed10c34439784fac51c9
root <10485@holbertstudents.com>
2025-05-07 17:11:22 +0000

    fix change code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-import sys
-import marshal
-import os
+import hidden_4
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.exit(1)
-
-    pyc_path = sys.argv[1]
-    if not os.path.isabs(pyc_path):
-        pyc_path = os.path.join(os.getcwd(), pyc_path)
-
-    with open(pyc_path, 'rb') as f:
-        f.seek(16)
-        code = marshal.load(f)
-
-    names = [name for name in code.co_names if not name.startswith('__')]
-    for name in sorted(names):
-        print(name)
+methods = dir(hidden_4)
+for names in methods:
+    if names[0] != '_':
+        print("{}".format(names))
