784d4e07db99bd0c720ca4aad5f3d78e8240a370
jerome244 <10485@holbertonstudents.com>
2025-05-07 11:27:54 +0200

    fix: rm chr and add exe

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):  # ASCII values of 'a' to 'z' are 97 to 122
-    if chr(i) != 'q' and chr(i) != 'e':  # Exclude 'q' and 'e'
-        print(chr(i), end="")
+print("".join("{:c}".format(i) for i in range(97, 123) if i != 101 and i != 113), end="")
