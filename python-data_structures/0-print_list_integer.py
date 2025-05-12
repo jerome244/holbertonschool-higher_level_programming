f989f196676cfc19e982ff986f760232133ec2f9
jerome244 <10485@holbertonstudents.com>
2025-05-12 15:33:57 +0200

    fix: rm descriptions

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,9 +2,5 @@
 
 
 def print_list_integer(my_list=[]):
-    """
-    Prints each integer in a list on its own.
-    line using str.format().
-    """
     for number in my_list:
         print("{}".format(number))
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,15 +2,6 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    """
-    Prints a matrix of integers.
-    Each integer is printed with str.format(), separated by a space.
-    After each row, prints a newline.
-    If no matrix is provided, prints an empty line.
-
-    Args:
-        matrix (list of lists of int): The 2D list of integers to print.
-    """
     for row in matrix:
         for idx, val in enumerate(row):
             end_char = " " if idx < len(row) - 1 else ""
