4e992890db6a80de188ffb9491cb5f30484e38b7
jerome244 <10485@holbertonstudents.com>
2025-05-12 13:52:12 +0200

    feat: find all multiple of 2 in a list, append it in an empty list and return it

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    """
+    Returns a list of booleans indicating 
+    if each integer in `my_list` is a multiple of 2.
+
+    Args:
+        my_list (list): List of integers.
+
+    Returns:
+        list: A new list of True/False values, same 
+        length as `my_list`.
+    """
+    result = []
+    for num in my_list:
+        # True if num is divisible by 2, False otherwise
+        result.append(num % 2 == 0)
+    return result
