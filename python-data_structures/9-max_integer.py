397e85e708a93bbb49e18039ad5bb63267e49e81
jerome244 <10485@holbertonstudents.com>
2025-05-12 13:47:24 +0200

    feat: finds the biggest value of a list and returns it

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """
+    Finds the biggest integer in a list.
+    If the list is empty, returns None.
+
+    Args:
+        my_list (list): List of integers.
+
+    Returns:
+        int or None: The biggest integer or None if the list is empty.
+    """
+    if len(my_list) == 0:
+        return None
+    max_val = my_list[0]
+    for num in my_list[1:]:
+        if num > max_val:
+            max_val = num
+    return max_val
