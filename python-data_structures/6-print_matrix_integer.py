924d7082528de12af72cea8d3b9a3e9803270c20
jerome244 <10485@holbertonstudents.com>
2025-05-12 12:52:17 +0200

    feat: print elements of a matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    Prints a matrix of integers.
+    Each integer is printed with str.format(), separated by a space. After each row, prints a newline.
+    If no matrix is provided, prints an empty line.
+
+    Args:
+        matrix (list of lists of int): The 2D list of integers to print.
+    """
+    for row in matrix:
+        for idx, val in enumerate(row):
+            end_char = " " if idx < len(row) - 1 else ""
+            print("{}".format(val), end=end_char)
+        print()
