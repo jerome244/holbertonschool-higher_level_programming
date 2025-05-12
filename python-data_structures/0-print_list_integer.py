7de837e968ffca92bc99f3a75a5893657f883365
jerome244 <10485@holbertonstudents.com>
2025-05-12 14:59:13 +0200

    fix: add a point in the description field

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_list_integer(my_list=[]):
     """
-    Prints each integer in a list on its own 
+    Prints each integer in a list on its own.
     line using str.format().
     """
     for number in my_list:
