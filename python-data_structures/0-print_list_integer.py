dd2af1fb347b45b91a56abec42db9a52e43080eb
jerome244 <10485@holbertonstudents.com>
2025-05-12 14:12:00 +0200

    fix: add comment

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
+    """
+    Prints each integer in a list on its own line using str.format().
+    """
     for number in my_list:
         print("{}".format(number))
