f36505a614710dd0f38133618b69734ca397ba96
jerome244 <10485@holbertonstudents.com>
2025-06-04 11:48:21 +0200

    feat: function for reading a text file and printing it in stdout + readme file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module provides a function to read and print the contents of a UTF8 text file.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its content to stdout.
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end='')
