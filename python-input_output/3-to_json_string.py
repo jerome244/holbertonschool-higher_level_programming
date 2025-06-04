1c361d25346f698696a649241ad1d0da9833ea46
jerome244 <10485@holbertonstudents.com>
2025-06-04 12:06:07 +0200

    feat: function to return a json representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a function that returns the JSON representation of an object (string).
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The Python object to convert.
+
+    Returns:
+        str: JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
