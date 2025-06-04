41bdea0e1741b9fc1d9cda164e4a200f10144f5e
jerome244 <10485@holbertonstudents.com>
2025-06-04 13:03:42 +0200

    feat: Return dictionary representation of Student instance

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Student module """
+
+class Student:
+    """ Student class """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation of Student instance.
+        If attrs is a list of strings, return only those attributes.
+        Otherwise, return all attributes.
+        """
+        if attrs is None:
+            return self.__dict__.copy()
+
+        filtered_dict = {}
+        for attr in attrs:
+            if attr in self.__dict__:
+                filtered_dict[attr] = self.__dict__[attr]
+        return filtered_dict
