3d40ad23d323dc86b6b4b7966e1201a10c139b23
jerome244 <10485@holbertonstudents.com>
2025-06-04 11:56:15 +0200

    feat: function to write a string into a text file (utf8)

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a function to write a string to a UTF8 text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: Number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
