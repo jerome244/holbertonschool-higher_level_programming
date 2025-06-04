0442bbccf31987294adc9b9b2bc30fd9e00fd0a9
jerome244 <10485@holbertonstudents.com>
2025-06-04 12:25:34 +0200

    fix: add encoding utf8

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+'''Module for load_to_json_file method.'''
 import json
 
+
 def load_from_json_file(filename):
-    """Creates an object from a JSON file"""
-    with open(filename, 'r') as f:
+    '''Method for loading from json file.'''
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
