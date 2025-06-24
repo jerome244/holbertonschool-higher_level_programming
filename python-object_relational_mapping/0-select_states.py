f43e395caca84cba835157e03a56f8915e05deef
jerome244 <10485@holbertonstudents.com>
2025-06-24 12:08:06 +0200

    fix: reorganized code presentation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,26 +2,24 @@
 """
 0-select_states script: lists all states from the specified MySQL database.
 """
-import MySQLdb
+
 import sys
+import MySQLdb
+
 
 def main():
     """Connect to MySQL, retrieve all states, and print each as a tuple."""
-    # Unpack command-line arguments
-    user, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
 
-    # Establish connection to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=user, passwd=password,
-                         db=database, charset="utf8")
+                         user=user, passwd=passwd,
+                         db=db_name, charset="utf8")
     cursor = db.cursor()
-
-    # Execute query and fetch results
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     for state in cursor.fetchall():
         print(state)
-
-    # Clean up
     cursor.close()
     db.close()
 
