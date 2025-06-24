96de778ce9528266ad1f5a96c99cad598988cceb
jerome244 <10485@holbertonstudents.com>
2025-06-24 13:11:19 +0200

    fix

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -4,7 +4,7 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
@@ -15,9 +15,13 @@ if __name__ == "__main__":
         f"mysql+mysqldb://{user}:{password}@localhost/{database}",
         pool_pre_ping=True
     )
+    # Ensure table exists
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Safe query to find the state by name
     state = session.query(State).filter(State.name == name).first()
     if state:
         print(state.id)
