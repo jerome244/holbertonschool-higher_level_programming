5e1ec5eb1dc98741bc10b635f219b871affc5df8
jerome244 <10485@holbertonstudents.com>
2025-06-24 20:23:51 +0200

    fix pycode

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""State class and Base = declarative_base() definition."""
+"""
+Module model_state
+Defines Base and State class linked to the 'states' table.
+"""
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+# Declarative base instance
 Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = "states"
+    """
+    State class mapped to 'states' table:
+    - id: auto-generated primary key
+    - name: non-nullable string up to 128 chars
+    """
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
