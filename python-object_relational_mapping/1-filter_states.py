26ffe721a396c10762c449d62172455d3142a333
jerome244 <10485@holbertonstudents.com>
2025-06-24 12:32:40 +0200

    fix: try other syntaxe

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
-"""
-1-filter_states.py: Lists all states with name starting with 'N' from the database.
-
-Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>
-"""
+"""Lists all states with a name starting with N from the database."""
 import MySQLdb
-from sys import argv
+import sys
 
-
-def main():
-    """Connect to MySQL and print states whose name starts with 'N'."""
-    # Unpack arguments
-    user, passwd, db_name = argv[1], argv[2], argv[3]
-
-    # Connect to database
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user,
-        passwd=passwd,
-        db=db_name,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
     )
     cur = conn.cursor()
-
-    # Execute query filtering names starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cur.execute(query)
-
-    # Fetch and print results
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     for row in cur.fetchall():
         print(row)
-
-    # Clean up
     cur.close()
     conn.close()
-
-
-if __name__ == "__main__":
-    main()
