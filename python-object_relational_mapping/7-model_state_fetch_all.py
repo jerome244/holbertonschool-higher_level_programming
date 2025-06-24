3dbdf8afdb8b2ad6ecc2f50d677431754d21c6fd
jerome244 <10485@holbertonstudents.com>
2025-06-24 13:05:10 +0200

    fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(State).order_by(State.id):
         print(f"{state.id}: {state.name}")
     session.close()
