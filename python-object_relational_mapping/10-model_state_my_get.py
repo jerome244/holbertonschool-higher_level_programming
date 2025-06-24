2f4ed38696b1acec5b664cd932e3b84f07f5c833
jerome244 <10485@holbertonstudents.com>
2025-06-24 13:12:00 +0200

    fix

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""10-model_state_my_get.py: Prints the State id of the State object with the given name from the database hbtn_0e_6_usa."""
+"""Return State object matching arg"""
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(1)
-
-    user, password, database, name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     engine = create_engine(
-        f"mysql+mysqldb://{user}:{password}@localhost/{database}",
-        pool_pre_ping=True
-    )
-    # Ensure table exists
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Safe query to find the state by name
-    state = session.query(State).filter(State.name == name).first()
-    if state:
-        print(state.id)
+    states = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
     else:
         print("Not found")
-
     session.close()
