a94e691a79da43ed32740ed36de04e7e8e8df537
jerome244 <10485@holbertonstudents.com>
2025-06-24 12:38:31 +0200

    feat(task2): handle an argument and display all values with matches arguments

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Lists all states with a given name from the database."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+    user, passwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db,
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cur.execute(query)
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
