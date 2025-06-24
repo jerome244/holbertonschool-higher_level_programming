76658585e653ade3f69505fd231d5d9310013d9c
jerome244 <10485@holbertonstudents.com>
2025-06-24 13:24:07 +0200

    fix

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Return State object matching arg"""
+"""lists all State objects from the database"""
 
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).\
-        filter(State.name == argv[4]).order_by(State.id).all()
-    if states:
-        print("{}".format(states[0].id))
-    else:
+    flag = 0
+    for state in session.query(State).order_by(State.id):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            flag = 1
+    if flag == 0:
         print("Not found")
     session.close()
