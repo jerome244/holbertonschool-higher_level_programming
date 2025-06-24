e7caae929d025097bb90f3a0fc61b9e18840bbeb
jerome244 <10485@holbertonstudents.com>
2025-06-24 19:21:51 +0200

    fix

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 """
 Module 3-say_my_name
-
-Provides function say_my_name that prints a formatted user name.
+Defines say_my_name(*args) to print names and handle missing args.
 """
 
-
-def say_my_name(first_name, last_name=""):
+def say_my_name(*args):
     """
-    Prints "My name is <first_name> <last_name>".
+    Print "My name is <first_name> <last_name>".
+
+    Args:
+        args[0] (str): first_name
+        args[1] (str): last_name (optional)
 
     Raises:
-        TypeError: if first_name or last_name is not a string.
+        TypeError: if first_name or last_name not strings,
+                  or if no arguments provided.
     """
-    if type(first_name) is not str:
+    if len(args) == 0:
+        raise TypeError(
+            "say_my_name() missing 2 required positional arguments: "
+            "'first_name' and 'last_name'"
+        )
+
+    first_name = args[0]
+    last_name = args[1] if len(args) > 1 else ""
+
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+
+    print(f"My name is {first_name} {last_name}")
