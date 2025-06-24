f023a7fb3dadfb6ad270533d536174a6a0cdf898
jerome244 <10485@holbertonstudents.com>
2025-06-24 19:54:40 +0200

    fix pycodestyle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -14,7 +14,8 @@ def pascal_triangle(n):
         for col_index in range(1, row_index):
             # Each element is sum of two elements above it
             rows[row_index][col_index] = (
-                rows[row_index - 1][col_index - 1] + rows[row_index - 1][col_index]
+                rows[row_index - 1][col_index - 1]
+                + rows[row_index - 1][col_index]
             )
 
     return rows
