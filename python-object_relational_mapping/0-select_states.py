d30a5f989a3191ae83385225f1c04a9dcb42b901
jerome244 <10485@holbertonstudents.com>
2025-06-24 18:17:27 +0200

    fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""
-Module containing function listing states from a database.
-"""
-import sys
+"""Lists all states from the database hbtn_0e_0_usa."""
+
 import MySQLdb
-
-
-def main():
-    """
-    Lists all states ordered by id in a database.
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306, user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+from sys import argv
 
 
 if __name__ == "__main__":
-    main()
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states "
+        "ORDER BY id ASC"
+    )
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
