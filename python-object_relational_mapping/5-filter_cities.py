82da44a6038d74d208ba42e9273d7ef899622ad4
jerome244 <10485@holbertonstudents.com>
2025-06-24 12:55:48 +0200

    fix

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""Lists all cities by state passed by user"""
+"""5-filter_cities.py: Lists all cities of a given state from the database hbtn_0e_4_usa."""
 
 import MySQLdb
-from sys import argv
+from sys import argv, exit
 
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    charset="utf8",
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3]
-)
-cur = conn.cursor()
-cur.execute("""
-    SELECT cities.name FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC;
-    """, (argv[4],))
-rows = cur.fetchall()
-print(", ".join([row[0] for row in rows]))
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    if len(argv) != 5:
+        exit(1)
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT cities.name FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC",
+        (argv[4],)
+    )
+    rows = cur.fetchall()
+    if rows:
+        print(", ".join([row[0] for row in rows]))
+    cur.close()
+    conn.close()
