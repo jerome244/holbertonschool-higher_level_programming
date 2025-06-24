eead8950558ce9e018dbf89ef789ea067bd1530b
jerome244 <10485@holbertonstudents.com>
2025-06-24 12:22:55 +0200

    fix: try to pass checker

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
-"""
-0-select_states.py: Connects to a MySQL database and lists all states sorted by their id.
+"""Lists all states from the database hbtn_0e_0_usa."""
 
-This script takes three arguments: MySQL username, password, and database name.
-It uses the MySQLdb module to fetch and display each state as a tuple.
-"""
-
-import sys
 import MySQLdb
-
-
-def main():
-    """Parse CLI arguments, connect to MySQL, execute query, and print states."""
-    if len(sys.argv) != 4:
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Establish connection to the MySQL server
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8",
-    )
-    cursor = connection.cursor()
-
-    # Retrieve all states ordered by id
-    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-
-    # Clean up database resources
-    cursor.close()
-    connection.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    main()
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        charset="utf8",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
